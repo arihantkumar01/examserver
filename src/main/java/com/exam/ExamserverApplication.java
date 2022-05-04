3e797d653d9847c75f2685a0ec8b591e564d0559
arihantkumar01 <arihantkumar0101@gmail.com>
2022-05-04 23:01:42 +0530

    service class and interface created, method to add user created

--- src/main/java/com/exam/ExamserverApplication.java
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -1,10 +1,22 @@
 package com.exam;
 
+import com.exam.entity.Role;
+import com.exam.entity.User;
+import com.exam.entity.UserRole;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
-public class ExamserverApplication {
+public class ExamserverApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 
@@ -12,4 +24,32 @@ public class ExamserverApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("starting code");
+
+		User user = new User();
+
+		user.setFirstName("Arihant");
+		user.setLastName("Kumar");
+		user.setUsername("arihantkumar");
+		user.setPassword("arihantkumar");
+		user.setEmail("arihantkumar@gmail.com");
+		user.setProfile("default.png");
+
+		Role role1 = new Role();
+		role1.setRoleId(44L);
+		role1.setRoleName("ADMIN");
+
+		Set<UserRole> userRoleSet = new HashSet<>();
+		UserRole userRole = new UserRole();
+
+		userRole.setRole(role1);
+		userRole.setUser(user);
+
+		userRoleSet.add(userRole);
+
+		User user1 = this.userService.creatUser(user, userRoleSet);
+		System.out.println(user1.getUsername());
+	}
 }
