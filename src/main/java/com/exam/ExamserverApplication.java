0a9f978b09495bad9d2417eacf7c8404285cd2eb
arihantkumar01 <arihantkumar0101@gmail.com>
2022-05-05 23:27:36 +0530

    REST API created to add user in database

--- src/main/java/com/exam/ExamserverApplication.java
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -28,28 +28,28 @@ public class ExamserverApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("starting code");
 
-		User user = new User();
-
-		user.setFirstName("Arihant");
-		user.setLastName("Kumar");
-		user.setUsername("arihantkumar");
-		user.setPassword("arihantkumar");
-		user.setEmail("arihantkumar@gmail.com");
-		user.setProfile("default.png");
-
-		Role role1 = new Role();
-		role1.setRoleId(44L);
-		role1.setRoleName("ADMIN");
-
-		Set<UserRole> userRoleSet = new HashSet<>();
-		UserRole userRole = new UserRole();
-
-		userRole.setRole(role1);
-		userRole.setUser(user);
-
-		userRoleSet.add(userRole);
-
-		User user1 = this.userService.creatUser(user, userRoleSet);
-		System.out.println(user1.getUsername());
+//		User user = new User();
+//
+//		user.setFirstName("Arihant");
+//		user.setLastName("Kumar");
+//		user.setUsername("arihantkumar");
+//		user.setPassword("arihantkumar");
+//		user.setEmail("arihantkumar@gmail.com");
+//		user.setProfile("default.png");
+//
+//		Role role1 = new Role();
+//		role1.setRoleId(44L);
+//		role1.setRoleName("ADMIN");
+//
+//		Set<UserRole> userRoleSet = new HashSet<>();
+//		UserRole userRole = new UserRole();
+//
+//		userRole.setRole(role1);
+//		userRole.setUser(user);
+//
+//		userRoleSet.add(userRole);
+//
+//		User user1 = this.userService.creatUser(user, userRoleSet);
+//		System.out.println(user1.getUsername());
 	}
 }
